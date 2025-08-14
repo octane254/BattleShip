28930a57f06a747137ead0063fa9b2327e848f3a
Eric Mbithi <mbithieri@gmail.com>
2025-08-14 16:27:04 +0300

    Initialize character and dragon stats with HP and damage values

---
+++ battlegame.py
@@ -0,0 +1,18 @@
+# characters (as strings):
+wizard = "Wizard"
+elf = "Elf"
+human = "Human"
+kalisi = "Kalisi"
+# character HP (as integers):
+wizard_hp = 150
+elf_hp = 100
+human_hp = 150
+kalisi_hp = 900
+# character Damage (as integers):
+wizard_damage = 150
+elf_damage = 100
+human_damage = 20
+kalisi_damage = 5
+# Dragon Stats
+dragon_hp = 300
+dragon_damage = 50
